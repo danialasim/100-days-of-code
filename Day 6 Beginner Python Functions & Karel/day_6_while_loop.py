fd9a7f8837279d8afe845386fe5b761632d0e1d2
Danial Asim <danialasimbashir@gmail.com>
2025-01-16 02:07:38 +0500

    learn about while loop in python

---
+++ Day 6 Beginner Python Functions & Karel/day_6_while_loop.py
@@ -0,0 +1,9 @@
+# Initialize the counter variable
+number = 6
+
+# While loop that continues as long as number is greater than 0
+# This creates a countdown from 6 to 1
+while number > 0:
+    print(number)    # Print current number
+    number -= 1      # Decrement number by 1 (same as number = number - 1)
+    # Loop continues until number becomes 0
