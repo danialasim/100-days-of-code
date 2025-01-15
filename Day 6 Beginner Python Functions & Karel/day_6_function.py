2621cbeb895f40460e45076be7ff26ea6b119dd4
Danial Asim <danialasimbashir@gmail.com>
2025-01-16 02:07:02 +0500

    learn about function in python

---
+++ Day 6 Beginner Python Functions & Karel/day_6_function.py
@@ -0,0 +1,11 @@
+# Define a simple function named my_function
+# Functions are blocks of reusable code that perform specific tasks
+def my_function():
+    # Function body - indented block of code
+    # These statements are executed when the function is called
+    print("Hello")  # First statement to execute
+    print("Bye")    # Second statement to execute
+
+
+# Call the function to execute its code block
+my_function()
