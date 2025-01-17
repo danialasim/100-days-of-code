12d1ccd10517cdf3ee7b57d677ed1711646dfc67
Danial Asim <danialasimbashir@gmail.com>
2025-01-17 17:58:21 +0500

    add hangman art for day 7

---
+++ Day 7 Beginner Hangman/day_7_hangman_art.py
@@ -0,0 +1,80 @@
+# Day 7 - Hangman Game Art Assets
+# This module contains the ASCII art for the game's visual elements
+
+#--------------------
+# Game Stages
+#--------------------
+
+# List of hangman stages, from complete (0 lives) to empty (6 lives)
+# Using raw strings (r'') to properly handle backslashes in ASCII art
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+#--------------------
+# Game Logo
+#--------------------
+
+# ASCII art logo for game start
+logo = r''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
