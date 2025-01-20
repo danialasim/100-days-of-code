c108a58bbf0a45528cc119764e8c9e91df710d6c
Danial Asim <danialasimbashir@gmail.com>
2025-01-21 00:54:50 +0500

    add ascii art for blackjack game

---
+++ Day 11 Beginner The Blackjack Capstone Project/day_11_black_jack_art.py
@@ -0,0 +1,18 @@
+# Day 11 - Blackjack Art
+# This module contains the ASCII art logo for the Blackjack game
+
+#--------------------
+# ASCII Logo
+#--------------------
+
+# Multi-line string using raw string notation (r"") to handle backslashes
+logo = r"""
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
