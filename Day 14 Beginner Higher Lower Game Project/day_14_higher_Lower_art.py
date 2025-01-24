e3636d7fb77df808ae261550389c95d2db5cf08d
Danial Asim <danialasimbashir@gmail.com>
2025-01-24 10:13:46 +0500

    add ascii art for higher lower game

---
+++ Day 14 Beginner Higher Lower Game Project/day_14_higher_Lower_art.py
@@ -0,0 +1,21 @@
+# ASCII art for the game logo - displayed at the start and after each guess
+logo = r"""
+    __  ___       __             
+   / / / (_)___ _/ /_  ___  _____
+  / /_/ / / __ `/ __ \/ _ \/ ___/
+ / __  / / /_/ / / / /  __/ /    
+/_/ ///_/\__, /_/ /_/\___/_/     
+   / /  /____/_      _____  _____
+  / /   / __ \ | /| / / _ \/ ___/
+ / /___/ /_/ / |/ |/ /  __/ /    
+/_____/\____/|__/|__/\___/_/     
+"""
+
+# ASCII art for the VS symbol - displayed between the two options
+vs = r"""
+ _    __    
+| |  / /____
+| | / / ___/
+| |/ (__  ) 
+|___/____(_)
+"""
